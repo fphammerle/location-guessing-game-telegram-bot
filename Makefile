ca0541b65c79d5a4db863d3a5ce095c32553715d
Fabian Peter Hammerle <fabian@hammerle.me>
2021-02-14 08:13:04 +0100

    added GPLv3+

--- Makefile
+++ Makefile
@@ -1,3 +1,20 @@
+# location-guessing-game-telegram-bot - Telegram Bot Sending Random Wikimedia Commons Photos
+#
+# Copyright (C) 2021 Fabian Peter Hammerle <fabian@hammerle.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # sync with https://github.com/fphammerle/switchbot-mqtt/blob/master/Makefile
 
 IMAGE_NAME = docker.io/fphammerle/location-guessing-game-telegram-bot
--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# location-guessing-game-telegram-bot - Telegram Bot Sending Random Wikimedia Commons Photos
+#
+# Copyright (C) 2021 Fabian Peter Hammerle <fabian@hammerle.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pathlib
 
 import setuptools
@@ -15,11 +32,13 @@ setuptools.setup(
     author_email="fabian+location-guessing@hammerle.me",
     url=_REPO_URL,
     project_urls={"Changelog": _REPO_URL + "/blob/master/CHANGELOG.md"},
+    license="GPLv3+",
     keywords=["bot", "game", "guessing", "location", "photos", "telegram",],
     classifiers=[
         # https://pypi.org/classifiers/
         "Development Status :: 3 - Alpha",
         "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Topic :: Games/Entertainment",
         # .github/workflows/python.yml TODO
